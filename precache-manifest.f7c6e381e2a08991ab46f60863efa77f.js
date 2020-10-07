a22037b4b378e9af18092e5752b5d75f6587e906
irlgabriel <radu.gabriel131@gmail.com>
2020-10-07 10:41:39 +0300

    Updates

---
+++ precache-manifest.f7c6e381e2a08991ab46f60863efa77f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca974baddec833342fc7946649c8a831",
+    "url": "/memory-card-game/index.html"
+  },
+  {
+    "revision": "9c3099731671dc523495",
+    "url": "/memory-card-game/static/js/2.05be29d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memory-card-game/static/js/2.05be29d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abbc0f5de491d0d47aba",
+    "url": "/memory-card-game/static/js/main.14ec6c6d.chunk.js"
+  },
+  {
+    "revision": "132bc5cce5f99c75ccf5",
+    "url": "/memory-card-game/static/js/runtime-main.8e3e6700.js"
+  }
+]);
